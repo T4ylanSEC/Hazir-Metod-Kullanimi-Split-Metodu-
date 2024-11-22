60b2ec098e52e25840fb4a7d2f356d12fbb2b20e
yaren <yaren@DESKTOP-9U2BG79>
2024-11-22 18:40:50 +0300

    Add project files.

---
+++ Hazir Metod Kullanimi ( Split Metodu)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazir_Metod_Kullanimi___Split_Metodu_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Split() Metodu kullanılacak.
+
+            Console.Write("Lütfen bir kelime girin: ");
+            string cumle = Console.ReadLine();
+
+            string[] kelimeler = cumle.Split(new char[] { ' ' });
+
+            int kelimeSayısı = kelimeler.Length;
+
+            Console.Write("Girdiğiniz cümlede kullanılan kelime sayısı: " + kelimeSayısı);
+
+            Console.ReadKey();
+        }
+    }
+}
